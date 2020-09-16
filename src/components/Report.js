fa1ebc33322c2c174034fe0506b0cb5f3ffa5b5e
Patrik Arvius <patrik.arvius@gmail.com>
2020-09-16 16:13:27 +0200

    Changed Report-component to fetch data from API

--- src/components/Report.js
+++ src/components/Report.js
@@ -2,37 +2,58 @@ import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown/with-html';
 
 class Report extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {md: null, id: null};
+    constructor() {
+        super()
+        this.state = {
+            md: null,
+            id: null,
+            error: null,
+            isLoaded: false
+        };
     }
 
     componentDidMount() {
-        this.getMarkdown();
+        this.getReport();
     }
 
     componentDidUpdate() {
         if (this.props.match.params.id !== this.state.id) {
-            this.getMarkdown();
+            this.getReport();
         }
     }
 
-    async getMarkdown() {
-        const file = await import(`../markdown/reports/week${this.props.match.params.id}.md`);
-
-        await fetch(file.default)
-            .then(response => response.text())
-            .then(text => {
-                this.setState({ md: text, id: this.props.match.params.id })
-            })
-    }
+    async getReport() {
+        await fetch(`http://localhost:1337/reports/week/${this.props.match.params.id}`)
+                .then(res => res.json())
+                .then(
+                    (res) => {
+                        this.setState({
+                            md: res.data.report_text,
+                            id: this.props.match.params.id,
+                            isLoaded: true,
+                        });
+                    },
+                    (error) => {
+                        this.setState({
+                            id: this.props.match.params.id,
+                            isLoaded: true,
+                            error
+                        });
+                    }
+                );
+            }
 
     render() {
+        const {error, md} = this.state;
+
+        if (error) {
+            console.log(error);
+            return <div>Error {error.message}</div>;
+        }
         return (
             <section>
                 <div className="Markdown-div">
-                    <ReactMarkdown source={this.state.md} escapeHtml={false} />
+                    <ReactMarkdown source={md} escapeHtml={false} />
                 </div>
             </section>
         );
